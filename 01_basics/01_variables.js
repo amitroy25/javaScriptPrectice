4d493a7654b3904d3e5cab290872710c6e356289
Amit <amitroy1223@gmail.com>
2024-08-10 16:56:18 +0530

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =44848 //cannot change state
+let accountEmail="amitroy2511@gmail.com" //varable declartion
+var accountPassword ="12345"  //variable declartion
+accountCity ="delhi"  //not a good prectice although we can declare value dirtection
+let accountState
+console.log(accountState)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+   prefred not to use var beacuse of issue of block scope and functional scope
+*/ 
+/*
+if we do not assign value to the varable than it is undefined bydefault
+*/ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //trean all js code as newer version
+//alert(3+3) //WE are using nodejs not browser
+
+console.log("amit")
+let name="amit"
+let age=23
+let isLoggeeIn=false
+//number=>2 to the power 53
+//bigint
+//string=>"" or''
+//boolean=>true/false
+//null=>standalone value
+//udefined=>undefined
+//symbol=>uniqeness
+//object
+console.log(typeof null) //output is object
+console.log(typeof undefined)// output is undefined
