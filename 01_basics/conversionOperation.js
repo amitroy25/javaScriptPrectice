62be15472fbd5a635c81cf69542a6807430d13ac
Amit <amitroy1223@gmail.com>
2024-08-12 16:05:20 +0530

    committing

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,17 @@
+
+let someString="47aaaa"
+
+let convertingIntoNumber=Number(someString)
+
+console.log(convertingIntoNumber)
+
+//"47"-> converting into number than easly converted but "47aa" ->converting into number than converted easly but value is Nan
+
+let someBoolean=true
+let convertingIntoDigi=Number(someBoolean)
+console.log(typeof convertingIntoDigi)
+
+//boolean is easly converted and showing right value 1 means true and 0 means false
+
+//number is also converted into to the string
+
