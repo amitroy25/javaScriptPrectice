bac56aff0d88e213c92e8c7fe9db912174773e0d
Amit <amitroy1223@gmail.com>
2024-08-13 14:54:35 +0530

    summary data types

---
+++ 01_basics/dataTypeSummary.js
@@ -0,0 +1,20 @@
+//primitive
+//7 types: String,Number,Boolean,null,undefined,Symbol,Bigint
+
+
+//refrenced type or non primitive dataType
+
+//Array,Object,Fucntions
+
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId)
+const city=["delhi","mumbai","banglore"]
+
+let person={
+
+    name: "amit roy",
+    age: 24
+}
